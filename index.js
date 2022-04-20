594c08f5bcb280eabb124a725b4be1affa13e8d0
Dipansh Saxena <depanshsaxena.ds@gmail.com>
2022-04-20 13:07:38 +0530

    make it better

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.use('/api/message',messageRoutes);
 const __dirname1 = path.resolve();
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(__dirname1 + "/vc-front/build"));
+    app.use(express.static(__dirname1 + "/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "vc-front","build","index.html"));
+        res.sendFile(path.resolve(__dirname1, "build","index.html"));
     });
 }else{
     app.use(express.static(__dirname1 + "/vc-front/public"));
